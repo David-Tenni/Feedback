5ee0397f177b02a266865484c3fe28b4b0eeed37
David Tenni <davidctenni@gmail.com>
2023-03-12 23:16:11 +1100

    added: main app component

--- feedback/src/index.js
+++ feedback/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<h1>Feedback</h1>);
+  .render(<App />);
